514bedf7cf06379239d273dcc139e0a107c9e7bd
Verena Severina <verenaseverina@gmail.com>
2020-02-06 00:21:02 +0700

    init progress-bar using react

---
+++ progress-bar-react/src/components/ProgressBar.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+import './ProgressBar.scss'
+
+export default function ProgressBar({
+  value,
+  limit
+}) {
+  const [width, setWidth] = useState(0)
+  const [percentage, setPercentage] = useState(0)
+
+  useEffect(() => {
+    if (value > 0 && value <= limit) {
+      setWidth((parseFloat(value) / limit) * 100)
+      setPercentage((parseFloat(value) / limit) * 100)
+    } else if (value <= 0) {
+      setWidth(0)
+      setPercentage(0)
+    } else {
+      setWidth(100)
+      setPercentage((parseFloat(value) / limit) * 100)
+    }
+  }, [value, limit]);
+
+  return (
+    <div className="progress-bar" >
+      <span className="filler-text">{percentage.toFixed(2)} %</span>
+      <div className="filler" style={{ width: `${width}%` }} />
+    </div>
+  )
+}
